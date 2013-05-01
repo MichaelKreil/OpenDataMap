84f31b159ffbe0bfec16794f8405824861653403
Micho <github@michael-kreil.de>
2013-05-01 16:14:59 +0200

    restructering api

--- server/modules/api.js
+++ server/modules/api.js
@@ -3,28 +3,52 @@ exports.Api = function (model) {
 	var log = new (require('./log.js').Log)('API');
 
 	me.get = function (path, callback) {
-		var includingNew = false;
-		if (path[1] == 'new') {
-			log.debug('requesting also new entries');
-			includingNew = true;
-		}
-		switch (path[0]) {
+		var source = path[0];
+
+		switch (source) {
 			case 'topics':
 				log.debug('requesting topics');
-				model.getTopics().getAll(includingNew, function (data) {
-					callback(JSON.stringify(data));
-				});
+				source = model.getTopics();
 			break;
 			case 'institutions':
 				log.debug('requesting institutions');
-				model.getInstitutions().getAll(includingNew, function (data) {
-					callback(JSON.stringify(data));
-				});
+				source = model.getInstitutions();
 			break;
 			default:
 				log.error('unknown request: '+path.join('/'));
 				callback('"error"');
 		}
+
+
+		var call = source.getAll;
+		var ids = [];
+		var includingNew = false;
+		var flags = path.splice(1);
+		for (var i = 0; i < flags.length; i++) {
+			var flag = flags[i];
+			switch (flag) {
+				case 'new':
+					includingNew = true;
+					log.debug('include new');
+				break;
+				case 'list':
+					call = source.getAll;
+					log.debug('as list');
+				break;
+				case 'tree':
+					call = source.getHierarchie;
+					log.debug('as tree');
+				break;
+				case '': /*ignore*/ break;
+				default:
+					log.warning('unknown Flag "'+flag+'"');
+			}
+		}
+
+		call(includingNew, function (data) {
+			callback(JSON.stringify(data));
+		});
+		
 		return path.join(',');
 	}
 	return me;
