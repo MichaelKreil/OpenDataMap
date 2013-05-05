aba415eff56d2d9cc88df47bcb259814cadc3697
Micho <github@michael-kreil.de>
2013-05-05 14:19:30 +0200

    moving compare functions in to the class

--- server/modules/model/mongodb.js
+++ server/modules/model/mongodb.js
@@ -31,7 +31,6 @@ exports.DB = function (config) {
 
 		collection.find(query, function (err, docs) {
 			if (err) log.error(err);
-			//console.log(docs);
 			callback(condense(docs, options));
 		});
 	}
@@ -79,7 +78,6 @@ exports.DB = function (config) {
 	me.setOne = function (entry, options, callback) {
 		log.debug('setOne ('+options.collectionName+')');
 		var collection = db.collection(options.collectionName);
-		console.log(entry);
 
 		var newEntry = {
 			attributes: entry.attributes,
@@ -139,61 +137,61 @@ exports.DB = function (config) {
 		return maxIds[collectionName];
 	}
 
+
+	var sameObject = function (obj1, obj2) {
+		if (!obj1.attributes && obj2.attributes) return false;
+		if (obj1.attributes && !obj2.attributes) return false;
+
+		var same = true;
+		var attr1 = obj1.attributes;
+		var attr2 = obj2.attributes;
+		if (!compareObjects(attr1, attr2)) same = false;
+		if (obj1.deleted !== obj2.deleted) same = false;
+		return same;
+	}
+
+	var compareObjects = function (obj1, obj2) {
+		var same = true;
+		for (var i in obj1) if (obj1.hasOwnProperty(i)) {
+			if (!compareValues(obj1[i], obj2[i])) same = false;
+		}
+		for (var i in obj2) if (obj2.hasOwnProperty(i)) {
+			if (!compareValues(obj1[i], obj2[i])) same = false;
+		}
+		return same;
+	}
+
+	var compareArrays = function (obj1, obj2) {
+		var same = true;
+		var n = Math.max(obj1.length, obj2.length);
+		for (var i = 0; i < n; i++) {
+			if (!compareValues(obj1[i], obj2[i])) same = false;
+		}
+		return same;
+	}
+
+	var compareValues = function (val1, val2) {
+		var datatype = Object.prototype.toString.call(val1);
+		switch (datatype) {
+			case '[object Array]':
+				return compareArrays(val1, val2);
+			break;
+			case '[object Object]':
+				return compareObjects(val1, val2);
+			break;
+			case '[object String]':
+			case '[object Number]':
+				return val1 == val2
+			break;
+			default:
+				log.error('Unknown data type "'+datatype+'"');
+				return val1 == val2
+		}
+	}
+
 	return me;
 }
 
-
-
-var sameObject = function (obj1, obj2) {
-	if (!obj1.attributes && obj2.attributes) return false;
-	if (obj1.attributes && !obj2.attributes) return false;
-
-	var same = true;
-	var attr1 = obj1.attributes;
-	var attr2 = obj2.attributes;
-	if (!compareObjects(attr1, attr2)) same = false;
-	if (obj1.deleted !== obj2.deleted) same = false;
-	return same;
-}
-
-var compareObjects = function (obj1, obj2) {
-	var same = true;
-	for (var i in obj1) if (obj1.hasOwnProperty(i)) {
-		if (!compareValues(obj1[i], obj2[i])) same = false;
-	}
-	for (var i in obj2) if (obj2.hasOwnProperty(i)) {
-		if (!compareValues(obj1[i], obj2[i])) same = false;
-	}
-	return same;
-}
-
-var compareArrays = function (obj1, obj2) {
-	var same = true;
-	var n = Math.max(obj1.length, obj2.length);
-	for (var i = 0; i < n; i++) {
-		if (!compareValues(obj1[i], obj2[i])) same = false;
-	}
-	return same;
-}
-
-var compareValues = function (val1, val2) {
-	var datatype = Object.prototype.toString.call(val1);
-	switch (datatype) {
-		case '[object Array]':
-			return compareArrays(val1, val2);
-		break;
-		case '[object Object]':
-			return compareObjects(val1, val2);
-		break;
-		case '[object String]':
-		case '[object Number]':
-			return val1 == val2
-		break;
-		default:
-			console.error('Unknown data type "'+datatype+'"');
-			return val1 == val2
-	}
-}
 
 
 
