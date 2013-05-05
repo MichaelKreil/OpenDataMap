48c9a9edf84045cfd2e925a62acd9ee56b5481b0
Micho <github@michael-kreil.de>
2013-05-05 14:16:40 +0200

    fixing bugs in Relations class

--- server/modules/model/relations.js
+++ server/modules/model/relations.js
@@ -11,7 +11,7 @@ exports.Relations = function (options) {
 		type:
 			comment
 			supportedBy
-			interested
+			interest
 			dataOfficial
 			dataInofficial
 			institutionContacted
@@ -38,39 +38,62 @@ exports.Relations = function (options) {
 		return list;
 	}
 
-	me.set = function (entry, options, callback) {
+	me.set = function (entries, options, callback) {
 		log.debug('set');
 
-		var stackLength = 0;
-		for (var key in relations) {
-			stackLength++;
+		var entryCount = entries.length;
+		entries.forEach(function (entry) {
+			me.setOne(entry, options, function (result) {
+				entryCount--;
+				if (entryCount == 0) callback(true);
+			});
+		});
+	}
+
+	me.setOne = function (entry, options, callback) {
+		log.debug('setOne');
+
+		stackLength = 0;
+		for (var key in relations) if (relations.hasOwnProperty(key)) stackLength++;
+		for (var key in relations) if (relations.hasOwnProperty(key)) {
 			(function () {
 				var myKey = key;
-				validateIntArray(entry.attributes[myKey], relations[myKey], function (list) {
+				validateIntArray(entry.attributes[myKey], myKey, function (list) {
 					entry.attributes[myKey] = list;
 					stackLength--;
 					if (stackLength == 0) {
 						options.collectionName = collectionName;
-						db.set(data, options, callback);
+						db.setOne(entry, options, callback);
 					}
 				});
 			})();
 		}
 	}
 
-	return me;
-}
-
-function validateIntArray(values, db, callback) {
-	if (!(Object.prototype.toString.call(value) == '[Object Array]')) callback([0]);
-	var newValues = [];
-	for (var i = 0; i < values.length; i++) {
-		var value = checkInt(values[i]);
-		if (!db.has({id:value})) value = undefined;
-		if (value !== undefined) newValues.push(value);
+	var validateIntArray = function(values, collectionName, callback) {
+		log.debug('validateIntArray ('+collectionName+': '+JSON.stringify(values)+')');
+		if (Object.prototype.toString.call(values) !== '[object Array]') {
+			log.debug('validateIntArray: not an Array');
+			callback([0]);
+			return;
+		}
+		var temp = [];
+		var stack = values.length;
+		values.forEach(function (value, index) {
+			db.has({id:value}, collectionName, function (result) {
+				if (result) temp[index] = value;
+				stack--;
+				if (stack == 0) {
+					var newValues = [];
+					for (var i = 0; i < temp.length; i++) {
+						if (temp[i] !== undefined) newValues.push(temp[i]);
+					}
+					if (newValues.length == 0) newValues = [0];
+					callback(newValues);
+				}
+			});
+		})
 	}
-	if (newValues.length == 0) newValues = [0];
-	return newValues;
 
 	var checkInt = function(value) {
 		if (isFinite(value)) return value;
